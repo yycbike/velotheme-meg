de1efd177c517fa092639feb1fb07293ac592d82
megdeutscher <devnull@localhost>
2012-02-19 15:29:42 -0800

    Fixed logo/small branding display

--- header.php
+++ header.php
@@ -106,7 +106,7 @@
 					<?php if(is_front_page()): ?>
 						<img class="raised-dropshadow" src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
 					<?php else: ?>
-						<img class="raised-dropshadow" src="http://beta.velo.mezzohost.com/wp-content/uploads/2012/02/Velopalooza-2012-poster-logo.png" width="<?php echo HEADER_IMAGE_WIDTH; ?>" alt="" />
+						<img class="poster-logo" src="http://beta.velo.mezzohost.com/wp-content/uploads/2012/02/Velopalooza-2012-poster-logo-trimmed.png" width="<?php echo HEADER_IMAGE_WIDTH; ?>" alt="" />
 					<?php endif; ?>
 				<?php /*endif*/; // end check for featured image or standard header ?>
 			</a>
