3e61adfd45e4bb92bee8a705178a97e50648fbf1
evan-dickinson <devnull@localhost>
2012-03-15 21:28:57 -0700

    Style the box about velopalooza

--- header.php
+++ header.php
@@ -126,11 +126,11 @@
 
 			<?php if(is_front_page())
 				{
-                    print '<div id=billboard class=featured-posts>';
                     // This was the featured post slideshow. Instead, we just put the body of one post on the
                     // front page.
+                    //print '<div id=billboard class=featured-posts>';
 					// include ('feature-slide.php');
-
+                    print '<div id=front-page-post>';
                     the_post();
                     the_content();
                     print '</div>';
