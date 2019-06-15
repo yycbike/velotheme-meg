38cfb7fabb408fce6b228a68e25fd94922e7e2f9
David Crosby <dave@dafyddcrosby.com>
2019-06-15 07:24:03 -0600

    Link to /previous-events instead

--- footer.php
+++ footer.php
@@ -30,8 +30,7 @@ Cyclepalooza would like to acknowledge that all events hosted take place on the 
 <br><br>
 </li>
 				<li>
-<a href="/cyclepalooza-2012">Cyclepalooza 2012 Archive</a> - <a href="/cyclepalooza-2013">Cyclepalooza 2013 Archive</a> - <a href="/winterpalooza-2014">Winterpalooza 2014 Archive</a> - <a href="/cyclepalooza-2014">Cyclepalooza 2014 Archive</a>
-<br><a href="/cyclepalooza-2015" title="Cyclepalooza 2015 - July 10 to 19">Cyclepalooza 2015 Archive</a> - <a href="/winterpalooza-2016-2" title="Winterpalooza 2016 - February 10 to 15">Winterpalooza 2016 Archive</a> - <a href="/cyclepalooza-2016" title="Cyclepalooza 2016 - July 1 to 10">Cyclepalooza 2016 Archive</a> 
+                <a href="/previous-events/">Previous Cyclepalooza Events</a>
 				</li>
 				</ul>
 
